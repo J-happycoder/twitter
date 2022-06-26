110cdf30ab5fa3ad53fde01eb26e33dbba55e496
이정우 <kyle@ijeong-uui-MacBookPro.local>
2022-06-26 09:00:05 +0900

    Initialize Firebase

---
+++ src/initializeFirebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCLn2luWtKJRi2XExxGsGXNjNiWtgcejKk",
+  authDomain: "twitter-1a50c.firebaseapp.com",
+  projectId: "twitter-1a50c",
+  storageBucket: "twitter-1a50c.appspot.com",
+  messagingSenderId: "364934388813",
+  appId: "1:364934388813:web:ff8951205bcad4b2b4d004",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
