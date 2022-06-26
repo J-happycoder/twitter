847fd209b5d0d9a15b181291b19de193a5f9e23e
이정우 <kyle@ijeong-uui-MacBookPro.local>
2022-06-26 09:39:26 +0900

    Securing Keys

--- src/initializeFirebase.js
+++ src/initializeFirebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCLn2luWtKJRi2XExxGsGXNjNiWtgcejKk",
-  authDomain: "twitter-1a50c.firebaseapp.com",
-  projectId: "twitter-1a50c",
-  storageBucket: "twitter-1a50c.appspot.com",
-  messagingSenderId: "364934388813",
-  appId: "1:364934388813:web:ff8951205bcad4b2b4d004",
+  apiKey: process.env.REACT_API_KEY,
+  authDomain: process.env.REACT_AUTH_DOMAIN,
+  projectId: process.env.REACT_PROJECT_ID,
+  storageBucket: process.env.REACT_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
